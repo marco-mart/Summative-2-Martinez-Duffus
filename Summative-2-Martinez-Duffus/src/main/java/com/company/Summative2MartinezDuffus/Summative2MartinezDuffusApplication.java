e27e8edecb133bd52e5747f8dcb8559427c06c61
marco-mart <martinez.marco.1027@gmail.com>
2023-03-03 16:37:49 -0700

    Initial commit

---
+++ Summative-2-Martinez-Duffus/src/main/java/com/company/Summative2MartinezDuffus/Summative2MartinezDuffusApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative2MartinezDuffus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative2MartinezDuffusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative2MartinezDuffusApplication.class, args);
+	}
+
+}
---
+++ Summative-2-Martinez-Duffus/src/test/java/com/company/Summative2MartinezDuffus/Summative2MartinezDuffusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative2MartinezDuffus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative2MartinezDuffusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
