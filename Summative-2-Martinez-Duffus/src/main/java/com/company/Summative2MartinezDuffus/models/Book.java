a3795ecfe06426cbe96c3d71b8a133d706397ac5
treyduffus <105085785+treyduffus@users.noreply.github.com>
2023-03-03 19:59:04 -0500

    models swag

---
+++ Summative-2-Martinez-Duffus/src/main/java/com/company/Summative2MartinezDuffus/models/Book.java
@@ -0,0 +1,121 @@
+package com.company.Summative2MartinezDuffus.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name="book")
+public class Book implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="book_id")
+    private Integer id;
+
+    @NotNull
+    private String isbn;
+
+    @NotNull
+    private Date publishDate;
+
+    @NotNull
+    private int authorId;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private int publisherId;
+
+    @NotNull
+    private BigDecimal price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(int publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return authorId == book.authorId && publisherId == book.publisherId && Objects.equals(id, book.id) && Objects.equals(isbn, book.isbn) && Objects.equals(publishDate, book.publishDate) && Objects.equals(title, book.title) && Objects.equals(price, book.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isbn, publishDate, authorId, title, publisherId, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", publishDate=" + publishDate +
+                ", authorId=" + authorId +
+                ", title='" + title + '\'' +
+                ", publisherId=" + publisherId +
+                ", price=" + price +
+                '}';
+    }
+}
