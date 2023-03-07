004b8c0d2beefca95116e62bd6bbc57bf4198c7e
treyduffus <105085785+treyduffus@users.noreply.github.com>
2023-03-06 19:53:34 -0500

    updating tests

--- Summative-2-Martinez-Duffus/src/test/java/com/company/Summative2MartinezDuffus/repository/BookRepositoryTest.java
+++ Summative-2-Martinez-Duffus/src/test/java/com/company/Summative2MartinezDuffus/repository/BookRepositoryTest.java
@@ -1,7 +1,13 @@
 package com.company.Summative2MartinezDuffus.repository;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class BookRepositoryTest {
 
 }
--- Summative-2-Martinez-Duffus/src/test/java/com/company/Summative2MartinezDuffus/repository/PublisherRepositoryTest.java
+++ Summative-2-Martinez-Duffus/src/test/java/com/company/Summative2MartinezDuffus/repository/PublisherRepositoryTest.java
@@ -1,7 +1,81 @@
 package com.company.Summative2MartinezDuffus.repository;
 
+import com.company.Summative2MartinezDuffus.models.Author;
+import com.company.Summative2MartinezDuffus.models.Book;
+import com.company.Summative2MartinezDuffus.models.Publisher;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class PublisherRepositoryTest {
+
+    @Autowired
+    PublisherRepository repo;
+
+    @Before
+    public void SetUp() throws Exception {
+        repo.deleteAll();
+    }
+
+    @Test
+    public void ShouldCreateAPublisher () throws Exception{
+        Publisher publisher = new Publisher();
+        Set<Book> books = new HashSet<>();
+
+        publisher.setName("epic");
+        publisher.setStreet("11213 epic street");
+        publisher.setBooks(books);
+        publisher.setState("CA");
+        publisher.setCity("LA");
+        publisher.setPostalCode("11123");
+        publisher.setPhone("123-456-789");
+        publisher.setEmail("epic@gmail.com");
+
+
+        publisher = repo.save(publisher);
+
+        Optional<Publisher> publisher1 = repo.findById(publisher.getId());
+
+        assertEquals(publisher1.get(),publisher);
+
+    }
+
+    @Test
+    public void ShouldUpdateAPublisher() throws Exception {
+        Publisher publisher = new Publisher();
+        Set<Book> books = new HashSet<>();
+
+        publisher.setName("epic");
+        publisher.setStreet("11213 epic street");
+        publisher.setBooks(books);
+        publisher.setState("CA");
+        publisher.setCity("LA");
+        publisher.setPostalCode("11123");
+        publisher.setPhone("123-456-789");
+        publisher.setEmail("epic@gmail.com");
+
+        Publisher publisher1 = repo.save(publisher);
+
+
+        publisher1.setName("swag");
+
+        Publisher publisher2 = repo.save(publisher1);
+
+        assertEquals(publisher2,publisher1);
+
+
+    }
+
 
 }
