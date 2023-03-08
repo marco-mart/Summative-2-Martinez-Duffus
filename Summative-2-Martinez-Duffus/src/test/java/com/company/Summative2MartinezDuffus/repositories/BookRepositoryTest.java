b50b7b101783c4d7f162844aae2e62ea6af2a69c
treyduffus <105085785+treyduffus@users.noreply.github.com>
2023-03-07 22:41:07 -0500

    adding book repo tests

--- Summative-2-Martinez-Duffus/src/test/java/com/company/Summative2MartinezDuffus/repositories/BookRepositoryTest.java
+++ Summative-2-Martinez-Duffus/src/test/java/com/company/Summative2MartinezDuffus/repositories/BookRepositoryTest.java
@@ -15,10 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.math.BigDecimal;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -134,10 +131,207 @@ public class BookRepositoryTest {
         book = repo.save(book);
 
 
+        Optional<Book> book1 = repo.findById(book.getId());
+
+        assertEquals(book1.get(),book);
+
+    }
+
+    @Test
+    public void ShouldUpdateABook () throws Exception {
+        Publisher publisher = new Publisher();
+        publisher.setName("Foxware");
+        publisher.setStreet("Ann Ave");
+        publisher.setCity("Los Angeles");
+        publisher.setState("CA");
+        publisher.setPostalCode("78952");
+        publisher.setPhone("1-(816)255-4849");
+        publisher.setEmail("bestpublisher@publishing.org");
+        //publisher.setBooks(new HashSet<>());
+
+        PublisherRepo.save(publisher);
+
+        Author author = new Author();
+        author.setFirstName("Mark");
+        author.setLastName("Epic");
+        author.setStreet("1112 street");
+        //books.add(book);
+        //author.setBooks(books);
+        author.setCity("Miami");
+        author.setState("FL");
+        author.setPostalCode("1111");
+        author.setPhone("123-456-7890");
+        author.setEmail("Mark@gmail.com");
+
+
+        author = AuthorRepo.save(author);
+
+        Book book = new Book();
+        book.setIsbn("1111111111");
+        book.setPublishDate(LocalDate.of(2012,10,12));
+        book.setTitle("Death");
+        book.setPublisherId(publisher.getId());
+        BigDecimal num = new BigDecimal("1.00");
+        book.setPrice(num);
+        book.setAuthorId(author.getId());
+
+
+
+        Book book1 = repo.save(book);
+
+        book.setTitle("Life");
+
+        Book book2 = repo.save(book1);
+
+        assertEquals(book1,book2);
+
+    }
+
+    @Test
+    public void ShouldDeleteABook () throws Exception {
+        Publisher publisher = new Publisher();
+        publisher.setName("Foxware");
+        publisher.setStreet("Ann Ave");
+        publisher.setCity("Los Angeles");
+        publisher.setState("CA");
+        publisher.setPostalCode("78952");
+        publisher.setPhone("1-(816)255-4849");
+        publisher.setEmail("bestpublisher@publishing.org");
+        //publisher.setBooks(new HashSet<>());
+
+        PublisherRepo.save(publisher);
+
+        Author author = new Author();
+        author.setFirstName("Mark");
+        author.setLastName("Epic");
+        author.setStreet("1112 street");
+        //books.add(book);
+        //author.setBooks(books);
+        author.setCity("Miami");
+        author.setState("FL");
+        author.setPostalCode("1111");
+        author.setPhone("123-456-7890");
+        author.setEmail("Mark@gmail.com");
+
+
+        author = AuthorRepo.save(author);
+
+        Book book = new Book();
+        book.setIsbn("1111111111");
+        book.setPublishDate(LocalDate.of(2012,10,12));
+        book.setTitle("Death");
+        book.setPublisherId(publisher.getId());
+        BigDecimal num = new BigDecimal("1.00");
+        book.setPrice(num);
+        book.setAuthorId(author.getId());
+
+        book = repo.save(book);
+
+        repo.deleteById(book.getId());
+
+        Optional<Book> book1 = repo.findById(book.getId());
+
+        assertFalse(book1.isPresent());
 
 
     }
 
+    @Test
+    public void ShouldFindABookById () throws Exception {
+        Publisher publisher = new Publisher();
+        publisher.setName("Foxware");
+        publisher.setStreet("Ann Ave");
+        publisher.setCity("Los Angeles");
+        publisher.setState("CA");
+        publisher.setPostalCode("78952");
+        publisher.setPhone("1-(816)255-4849");
+        publisher.setEmail("bestpublisher@publishing.org");
+        //publisher.setBooks(new HashSet<>());
+
+        PublisherRepo.save(publisher);
+
+        Author author = new Author();
+        author.setFirstName("Mark");
+        author.setLastName("Epic");
+        author.setStreet("1112 street");
+        //books.add(book);
+        //author.setBooks(books);
+        author.setCity("Miami");
+        author.setState("FL");
+        author.setPostalCode("1111");
+        author.setPhone("123-456-7890");
+        author.setEmail("Mark@gmail.com");
+
+
+        author = AuthorRepo.save(author);
+
+        Book book = new Book();
+        book.setIsbn("1111111111");
+        book.setPublishDate(LocalDate.of(2012,10,12));
+        book.setTitle("Death");
+        book.setPublisherId(publisher.getId());
+        BigDecimal num = new BigDecimal("1.00");
+        book.setPrice(num);
+        book.setAuthorId(author.getId());
+
+        book = repo.save(book);
+
+        Optional<Book> book1 = repo.findById(book.getId());
+
+        assertEquals(book1.get(),book);
+
+    }
+
+    @Test
+    public void ShouldFindAllBooks () throws Exception {
+        Publisher publisher = new Publisher();
+        publisher.setName("Foxware");
+        publisher.setStreet("Ann Ave");
+        publisher.setCity("Los Angeles");
+        publisher.setState("CA");
+        publisher.setPostalCode("78952");
+        publisher.setPhone("1-(816)255-4849");
+        publisher.setEmail("bestpublisher@publishing.org");
+        //publisher.setBooks(new HashSet<>());
+
+        PublisherRepo.save(publisher);
+
+        Author author = new Author();
+        author.setFirstName("Mark");
+        author.setLastName("Epic");
+        author.setStreet("1112 street");
+        //books.add(book);
+        //author.setBooks(books);
+        author.setCity("Miami");
+        author.setState("FL");
+        author.setPostalCode("1111");
+        author.setPhone("123-456-7890");
+        author.setEmail("Mark@gmail.com");
+
+
+        author = AuthorRepo.save(author);
+
+        Book book = new Book();
+        book.setIsbn("1111111111");
+        book.setPublishDate(LocalDate.of(2012,10,12));
+        book.setTitle("Death");
+        book.setPublisherId(publisher.getId());
+        BigDecimal num = new BigDecimal("1.00");
+        book.setPrice(num);
+        book.setAuthorId(author.getId());
+
+        book = repo.save(book);
+
+        List<Book> bookList = new ArrayList<>();
+
+        bookList.add(book);
+
+        List<Book> bookList1 = repo.findAll();
+
+        assertEquals(bookList1,bookList);
+
+
+    }
 
 
 }
